41856e0e4537ddec3f7f01762a39922efb87864a
Gabriel Faro <faro@Mac-mini-de-Gabriel.local>
2018-04-10 19:42:28 -0300

    bug fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,8 @@
-import React, { Component } from "react";
+import React, { Component } from "react"
 import facebook from "../assets/facebook.svg"
 import instagram from "../assets/instagram.svg"
 import git from "../assets/git.svg"
+import copyright from "../assets/copyright.svg"
 
 export default class Footer extends Component {
   render() {
@@ -20,13 +21,14 @@ export default class Footer extends Component {
         }}
       >
         <div style={{width: '100%', display: 'flex', marginLeft:'2%', marginRight: '2%', fontSize:'75%' }}>
-          <span style={{ flex: 1, color: "white", marginTop: 7 }}>
+        <img src={copyright} alt={'copyright-symbol'} style={{marginTop: 7, marginRight: 5, width: 13, height: 13}}/>
+          <div style={{ flex: 1, color: "white", marginTop: 7 }}>                            
             Powered by Me Myself
-          </span>          
+          </div>          
 
-          <img src={git} alt={'github'} style={{marginLeft: 10, width: 25, height: 25}}/>
-          <img src={instagram} alt={'instagram'} style={{marginLeft: 10, width: 25, height: 25}}/>
-          <img src={facebook} alt={'facebook'} style={{marginLeft: 10, width: 25, height: 25}}/>
+          <img src={git} alt={'github-symbol'} style={{marginLeft: 10, width: 25, height: 25}}/>
+          <img src={instagram} alt={'instagram-symbol'} style={{marginLeft: 10, width: 25, height: 25}}/>
+          <img src={facebook} alt={'facebook-symbol'} style={{marginLeft: 10, width: 25, height: 25}}/>
 
         </div>  
       </footer>
