5ae4b3fd803775e811c1374a28311d40d1e60115
Gabriel Faro <gabrielfaro@Mac-mini-de-Gabriel.local>
2018-04-16 15:29:54 -0300

    Test

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ class App extends Component {
                 <b>Lorem ipsum dolor sit amet, consectetur adipiscing elit</b>.
                 Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris
                 massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti
+                <b>Lorem ipsum dolor sit amet, consectetur adipiscing elit</b>.
                 sociosqu ad litora torquent per conubia nostra, per inceptos
                 himenaeos. Curabitur sodales ligula in libero. Sed dignissim
                 lacinia nunc.
